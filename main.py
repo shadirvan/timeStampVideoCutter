8b4340c2981a7bb2a53494291b1c154e5ee811dc
Shadirvan <shadirvan2021@gmail.com>
2023-01-23 19:42:06 +0530

    added youtube download module

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from pytube import YouTube
 import csv
 with open("timeStamps.csv", 'r') as file:
   csvreader = csv.reader(file)
-  for row in csvreader:
-    print(row[0])
+#   for row in csvreader:
+#     print(row[0])
+
+
+
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    print("Please wait while the file is downloaded....")
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("Download Failed !")
+    print("Download completed successfully")
+
+
+link = input("Enter the YouTube video URL: ")
+Download(link)
