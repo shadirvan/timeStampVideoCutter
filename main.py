1378b0be0b32e5829bd22c436430f15e7f21381c
Shadirvan <shadirvan2021@gmail.com>
2023-01-23 22:41:42 +0530

    access csv and convert into sec

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 from pytube import YouTube
+from moviepy.editor import *
 import csv
+
+    
+    
+
+
+
+
+# def Download(link):
+#     youtubeObject = YouTube(link)
+#     print("Please wait while the file is downloaded....")
+#     youtubeObject = youtubeObject.streams.get_highest_resolution()
+#     try:
+#         youtubeObject.download(filename="full.mp4")
+#     except:
+#         print("Download Failed !")
+#     print("Download completed successfully")
+
 with open("timeStamps.csv", 'r') as file:
   csvreader = csv.reader(file)
-#   for row in csvreader:
-#     print(row[0])
+  timeList = []
+  for row in csvreader:
+    hour = int(row[0][0:2]) * 3600
+    minute = int(row[0][3:5]) * 60
+    second = int(row[0][6:8]) 
+    timeInSec = hour + minute + second
+    timeList.append(timeInSec)
 
 
+    
 
 
-def Download(link):
-    youtubeObject = YouTube(link)
-    print("Please wait while the file is downloaded....")
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
-    try:
-        youtubeObject.download()
-    except:
-        print("Download Failed !")
-    print("Download completed successfully")
+def CutVideo(initialTime, finalTime):
+    # loading video gfg
+    clip = VideoFileClip("full.mp4")
+    # getting only first 5 seconds
+    clip = clip.subclip(initialTime, finalTime)
+    # showing clip
+    clip.write_videofile("clip.mp4")
 
 
-link = input("Enter the YouTube video URL: ")
-Download(link)
+# link = input("Enter the YouTube video URL: ")
+# Download(link)
+
