1c14beebe7cb3da7e322f08189135e7372fa549d
Shadirvan <shadirvan2021@gmail.com>
2023-01-24 00:03:27 +0530

    added core functions

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from pytube import YouTube
 from moviepy.editor import *
 import csv
 
-    
-    
-
-
-
 
 # def Download(link):
 #     youtubeObject = YouTube(link)
@@ -18,29 +13,30 @@ import csv
 #         print("Download Failed !")
 #     print("Download completed successfully")
 
-with open("timeStamps.csv", 'r') as file:
-  csvreader = csv.reader(file)
-  timeList = []
-  for row in csvreader:
-    hour = int(row[0][0:2]) * 3600
-    minute = int(row[0][3:5]) * 60
-    second = int(row[0][6:8]) 
-    timeInSec = hour + minute + second
-    timeList.append(timeInSec)
-
-
+# with open("timeStamps.csv", 'r') as file:
+#   csvreader = csv.reader(file)
+#   timeList = []
+#   nameList =[]
+#   for row in csvreader:
+#     hour = int(row[0][0:2]) * 3600
+#     minute = int(row[0][3:5]) * 60
+#     second = int(row[0][6:8]) 
+#     timeInSec = hour + minute + second
+#     timeList.append(timeInSec)
+#     nameList.append(row[1])
+# def CutVideo(initialTime, finalTime, fileName):
+   
+clip = VideoFileClip("full.mp4")
+print(clip.duration)
     
-
-
-def CutVideo(initialTime, finalTime):
-    # loading video gfg
-    clip = VideoFileClip("full.mp4")
-    # getting only first 5 seconds
-    clip = clip.subclip(initialTime, finalTime)
-    # showing clip
-    clip.write_videofile("clip.mp4")
+    # clip = clip.subclip(initialTime, finalTime)
+    
+    # clip.write_videofile(str(fileName) + ".mp4")
 
 
 # link = input("Enter the YouTube video URL: ")
 # Download(link)
+
+# for i in range(0,len(timeList)-1):
+#     CutVideo(timeList[i],timeList[i+1],nameList[i])
 
