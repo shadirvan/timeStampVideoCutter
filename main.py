dde983788e6797ab2f62007db81fd842a7de340b
Shadirvan <shadirvan2021@gmail.com>
2023-01-24 00:12:55 +0530

    core functionality implemented

--- main.py
+++ main.py
@@ -3,40 +3,36 @@ from moviepy.editor import *
 import csv
 
 
-# def Download(link):
-#     youtubeObject = YouTube(link)
-#     print("Please wait while the file is downloaded....")
-#     youtubeObject = youtubeObject.streams.get_highest_resolution()
-#     try:
-#         youtubeObject.download(filename="full.mp4")
-#     except:
-#         print("Download Failed !")
-#     print("Download completed successfully")
+def Download(link):
+    youtubeObject = YouTube(link)
+    print("Please wait while the file is downloaded....")
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download(filename="full.mp4")
+    except:
+        print("Download Failed !")
+    print("Download completed successfully")
 
-# with open("timeStamps.csv", 'r') as file:
-#   csvreader = csv.reader(file)
-#   timeList = []
-#   nameList =[]
-#   for row in csvreader:
-#     hour = int(row[0][0:2]) * 3600
-#     minute = int(row[0][3:5]) * 60
-#     second = int(row[0][6:8]) 
-#     timeInSec = hour + minute + second
-#     timeList.append(timeInSec)
-#     nameList.append(row[1])
-# def CutVideo(initialTime, finalTime, fileName):
-   
-clip = VideoFileClip("full.mp4")
-print(clip.duration)
-    
-    # clip = clip.subclip(initialTime, finalTime)
-    
-    # clip.write_videofile(str(fileName) + ".mp4")
+with open("timeStamps.csv", 'r') as file:
+  csvreader = csv.reader(file)
+  timeList = []
+  nameList =[]
+  for row in csvreader:
+    hour = int(row[0][0:2]) * 3600
+    minute = int(row[0][3:5]) * 60
+    second = int(row[0][6:8]) 
+    timeInSec = hour + minute + second
+    timeList.append(timeInSec)
+    nameList.append(row[1])
+def CutVideo(initialTime, finalTime, fileName):
+   clip = VideoFileClip("full.mp4")
+   clip = clip.subclip(initialTime, finalTime)
+   clip.write_videofile(str(fileName) + ".mp4")
 
 
-# link = input("Enter the YouTube video URL: ")
-# Download(link)
+link = input("Enter the YouTube video URL: ")
+Download(link)
 
-# for i in range(0,len(timeList)-1):
-#     CutVideo(timeList[i],timeList[i+1],nameList[i])
+for i in range(0,len(timeList)-1):
+    CutVideo(timeList[i],timeList[i+1],nameList[i])
 
